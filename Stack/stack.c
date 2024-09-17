28c17214d84e25a056f2d78867d939eb31eb3d37
pranav <pranavprasad.r500@gmail.com>
2024-09-17 09:32:57 +0530

    implementing functions pop, push, scan, clear, display

--- Stack/stack.c
+++ Stack/stack.c
@@ -1,9 +1,11 @@
 #include <stdio.h> // the header files
-
+#include <stdlib.h> //required for implementing the scan function
 
 int pop();         // function prototypes
 int push(int);
 int display();
+                    //quality of life features
+int scan(char*);
 void clear();
 
 #define MAXSIZE 10
@@ -13,7 +15,7 @@ int top = -1; // defining the top pointer to keep track of stack overflow/underf
                         //(not actually a pointer variable)
 
 int main(int argc, char* argv[]){   // main function
-
+    
     return 0;
 }
 
@@ -37,17 +39,53 @@ int pop(){
     return 1;
 }
 
-int push(int){
-    //TODO push function
-    
+int push(int item){
+    if(top == MAXSIZE - 1) // checking overflow condition
+    {
+        printf("\nThe Stack is Full\n");
+        return 0;
+    }
+    else
+    {
+        stackArray[++top] = item; // inserting the item  at array index of top after incrementing it
+        printf("Inserted: %d", item);
+        return 0;
+    }
     return 1;
 }
 
 int display(){
-    //TODO display function
+
+    if(top == -1)
+    {
+        printf("Stack is empty");
+        return 0;
+    }
+    else
+    {
+        for(int i = top; i > -1; i--)
+        {
+            printf("%d : %d \n", i , stackArray[i]);
+        }
+        return 0;
+    }
     return 1;
 }
 
 void clear(){
-    //TODO clear function
+    system("clear");
+}
+
+int scan(char* s)
+{ 
+    char buffer[100];
+
+    printf("%s :", s);
+
+    if(fgets(buffer, sizeof(buffer), stdin) != NULL)
+    {
+        return atoi(buffer);
+    }
+   
+    return -1;
 }
