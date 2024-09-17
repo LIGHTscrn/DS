c76707f8f25d3c07b6d7534da6ab4cb1416eead8
pranav <pranavprasad.r500@gmail.com>
2024-09-17 11:38:37 +0530

    created linearsearch function without recursion

--- Search/linearsearch.c
+++ Search/linearsearch.c
@@ -2,5 +2,19 @@
 
 int main(void)
 {
+
     return 0;
+}
+
+void linearsearch(int arr[], int n, int key)// function to implement linearsearch taking arguments (array[], array length, element to serach)
+{
+    for(int i = 0; i < n; i++)
+    {
+        if(arr[i] == key)
+        {
+            printf("Element found at index %d\n", i);
+            return;
+        }
+    }
+    printf("Element not found\n");
 }
