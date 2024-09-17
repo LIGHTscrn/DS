17642258fef5fc5c45a234e3d70a24f3cd3689bc
pranav <pranavprasad.r500@gmail.com>
2024-09-17 12:05:57 +0530

    Defining recursive linear search

--- Search/linearsearch.c
+++ Search/linearsearch.c
@@ -1,8 +1,33 @@
 #include <stdio.h>
 
+void linearsearch(int arr[], int n, int key);
+void Recursive_linearsearch(int arr[], int n, int key);
+void Recursive_linearsearch_helper(int arr[], int n, int key, int i);
+
 int main(void)
 {
+    printf("Linear Search\n");
 
+    printf("Enter the array size: ");// get the size of teh array from user
+    int length;
+    scanf("%d", &length);
+
+    int LinearSearchArray[length];// declare the array of size length
+
+    printf("Enter the elemnts in the array \n");// initialize the array
+    
+    for(int i = 0; i< length; i++)
+    {
+        printf("Index %d : ", i);
+        scanf("%d", &LinearSearchArray[i]); // get the elements from the user
+    }
+
+    printf("Enter the element to search for : "); // get the element to search for from the user 
+    
+    int element;
+    scanf("%d", &element);
+    linearsearch(LinearSearchArray, length, element); // call the linearsearch function
+    Recursive_linearsearch(LinearSearchArray, length, element); // call the recursive linearsearch function
     return 0;
 }
 
@@ -17,4 +42,26 @@ void linearsearch(int arr[], int n, int key)// function to implement linearsearc
         }
     }
     printf("Element not found\n");
+}
+
+void Recursive_linearsearch(int arr[], int n, int key)//function to implement linearsearch
+{
+    Recursive_linearsearch_helper(arr, n, key, 0);
+}
+void Recursive_linearsearch_helper(int arr[], int n, int key, int i)
+{
+    if(i == n)
+    {
+        printf("Element not found\n");
+        i=0;
+        return;
+    }
+    if(arr[i] == key)
+    {
+        printf("Element found at index %d\n", i);
+        i=0;
+        return;
+    }
+    i++;
+    Recursive_linearsearch_helper(arr,n,key,i);
 }
