2165cdce5aca21d0d01962b762e568756c631921
pranav <pranavprasad.r500@gmail.com>
2024-09-17 08:49:45 +0530

    Creating Stack using arrays and linked list

---
+++ Stack/stack.c
@@ -0,0 +1,32 @@
+#include <stdio.h> // the header files
+
+
+int pop();         // function prototypes
+int push(int);
+int display();
+void clear();
+
+
+int main(int argc, char* argv[]){   // main function
+
+    return 0;
+}
+
+int pop(){
+    //TODO pop function
+    return 1;
+}
+
+int push(int){
+    //TODO push function
+    return 1;
+}
+
+int display(){
+    //TODO display function
+    return 1;
+}
+
+void clear(){
+    //TODO clear function
+}
