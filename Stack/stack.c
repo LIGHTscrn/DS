fe37d17b0479b48345b837b32516410330e6e0c9
pranav <pranavprasad.r500@gmail.com>
2024-09-17 09:02:12 +0530

    implementing the pop function in stack

--- Stack/stack.c
+++ Stack/stack.c
@@ -6,6 +6,11 @@ int push(int);
 int display();
 void clear();
 
+#define MAXSIZE 10
+int stackArray[MAXSIZE]; // the stack array of size MAXSIZE
+
+int top = -1; // defining the top pointer to keep track of stack overflow/underflow, 
+                        //(not actually a pointer variable)
 
 int main(int argc, char* argv[]){   // main function
 
@@ -13,12 +18,28 @@ int main(int argc, char* argv[]){   // main function
 }
 
 int pop(){
-    //TODO pop function
+    // checking underflow condition
+    if(top == -1)
+    {
+        printf("stack is empty");
+        return 0;
+    }
+    else
+    {
+        int item = stackArray[top]; //storing the element to be deleted in an varialbe
+
+        stackArray[top--] = 0; // replacing the value at top index of array to 0 and decrementing top
+
+        printf("DELETED : %d ", item);
+
+        return 0; //returning the deleted element
+    }
     return 1;
 }
 
 int push(int){
     //TODO push function
+    
     return 1;
 }
 
