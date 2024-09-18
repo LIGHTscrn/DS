c3241c25d7bf1da88bc1d2afcbf96853b1742147
pranav <pranavprasad.r500@gmail.com>
2024-09-18 14:35:42 +0530

    declared a matrix with user chosen rows and columns

---
+++ SparseMatrix/sparsematrix.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int rows , cols;
+
+    printf("Enter the number of rows of the matrix: ");
+    scanf("%d", &rows);
+
+    printf("Enter the number of columns of the matrix: ");
+    scanf("%d", &cols);
+
+    int matrix[rows][cols];
+
+    
+}
