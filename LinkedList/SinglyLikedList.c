bfc7b401df65d90660a55ac7200c4c78fd68f771
LIGHTscrn <pranavprasad.r500@gmail.com>
2024-10-08 19:27:45 +0530

    Corrected typo in scanf function and added insertion at beginning functionality

--- LinkedList/SinglyLikedList.c
+++ LinkedList/SinglyLikedList.c
@@ -11,9 +11,22 @@ Node *head = NULL;
 int main(int argc , char * argv[])
 {
     printf("Linked List\n");
-
+    int data;
     while(1){
-        printf("")
-    l}
-    return 0;
+        printf("Insert \n");
+        printf("1.At the Beginning \t 2.At the End \t 3.At specific location\n");
+        printf("Delete \n");
+        printf("4.At the Beginning \t 5.At the End \t 6.At specific location\n");
+        printf("7.Display \t 8.Exit\n");
+        int choice;       
+        printf("Enter your choice : ");
+        scanf("%d",&choice);
+        switch(choice){
+            case 1:
+                printf("Enter the data : ");
+                scnaf("%d",&data);
+                insertAtBeginning(data);
+        }
+         }
+   return 0;
 }
