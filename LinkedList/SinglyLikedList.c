0696e006ff243388d5b5c3e0eaf9f4792fad67ce
LIGHTscrn <pranavprasad.r500@gmail.com>
2024-10-08 18:59:26 +0530

    Linked list files

---
+++ LinkedList/SinglyLikedList.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+typedef struct Node {
+    int data;
+    struct node *next;
+} Node;
+
+Node *head = NULL;
+
+int main(int argc , char * argv[])
+{
+    printf("Linked List\n");
+
+    while(1){
+        printf("")
+    l}
+    return 0;
+}
