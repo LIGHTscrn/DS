cec15ad75e07e90466e16cd963d2fe56d46861bd
Pranav Rajendraprasad <pranavprasad.r500@gmail.com>
2024-11-02 11:11:19 +0000

    Deletion first half

--- BST/bst.c
+++ BST/bst.c
@@ -43,6 +43,30 @@ void Insert(int data){
     else{
         parent -> right = newnode;
     }
+}
 
+void delete(int data){
+    node* current = root;
+    node* parent = NULL;
+    if(root == NULL){
+        printf("Tree is empty");
+        return;
+    }
+    while(current){
+        parent = current;
+        if(data == (current -> data)){
+            break;
+        }
+        else if(data < (current -> data)){
+            current = (current -> left);
+        }
+        else if(data > (current -> data)){
+            current = (current -> right);
+        }
+    }
+    if(current == NULL){
+        printf("Element not found");
+        return;
+    }
     
 }
