1a273698646585662ee2b383fcce635aad7512a0
LIGHTscrn <pranavprasad.r500@gmail.com>
2024-11-10 08:24:48 +0530

    Dfs algorithm done

--- Graph/DFS.c
+++ Graph/DFS.c
@@ -1,6 +1,45 @@
 #include<stdio.h>
 
-int adj[10][10], visited[10], n = 0, V = 0;
+int adj[10][10], visited[10], n = 0, V = 0, S[10];
+
+int top = -1;
+int pop(int S[]){
+    if(top == -1){
+        return -1;
+    }
+    else{
+        return S[top--];
+    }
+}
+
+int push(int S[], int element){
+    if(top == V-1){
+        reuturn -1;
+    }
+    else{
+        S[top++] = element;
+        retunr 0;
+    }
+}
+
+void DFS(int source){
+    visited[source] = 1;
+    if(push(S, source) == -1){
+        printf("Stack overflow");
+    }
+    int j = 0;
+    for(j = 0; j < V; j++){
+        if(adj[source][j] == 1 && visited[j] == 0){
+            count++;
+            if(push(S, j) == -1){
+                printf("Stack overflow");
+                return;
+            }
+            DFS(j);
+        }
+    }
+    pop(S);
+}
 
 int main(void)
 {
@@ -24,4 +63,6 @@ int main(void)
         scanf("%d %d", &u, &v);
         adj[u][v] = 1;
     }
+
+    DFS(source);
 }
